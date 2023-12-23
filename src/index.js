abcb8ff3b50e868319e3e99d4b2b47b8c8dae93e
Babyliy <olalekanaliyah@gmail.com>
2023-12-23 15:38:26 -0700

    Added the Search engine functionality and API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let h1 = document.querySelector("#city");
+  h1.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "edt5131fa5d94b5bd8ef44o10fc9b9e2";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search");
-  let h1 = document.querySelector("#city");
-  h1.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSearch);
+
+searchCity("Lisbon");
