9e0ac84bfccf14dacc8d73d74e2a1875f96f5c21
Babyliy <olalekanaliyah@gmail.com>
2023-12-24 17:24:28 -0700

    updated the javascript

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let h1 = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.date.time * 1000);
+  timeElement.innerHTML = `${date.getDay} ${date.getHours}:${date.getMinutes}`;
   h1.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 }
 
 function searchCity(city) {
